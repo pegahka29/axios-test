32edad8b4a83fc6fb9af6456e2ed8504c66460d1
Pegah Kazemi <p.kazemi@Rayan.Parsi>
2023-07-23 15:49:12 +0330

    add vuex

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+//import the createStore object from Vuex
+import { createStore } from 'vuex'
+// Import axios to make HTTP requests
+// import axios from "axios"
+export default createStore({
+    state: {},
+    getters: {},
+    actions: {},
+    mutations: {}
+})
+/** we have just created a boiler plate for our vuex store module**/
